5a39bc64e610e7962094a1a9e066c724aa7379c4
Jonathan Raviotta <jraviotta@gmail.com>
2018-03-19 10:54:00 -0400

    initial commmit

--- .tokens.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export GITHUB_TOKEN=1278488ce4a393eb06e11aea64f313afd9342b2d
-export CIRCLE_TOKEN=4e122b4442accb27a7658ce8f5382f661f11bf16
-export TERMINUS_TOKEN=Xg7kSyQeH0cSMHveMe0WOCEdqj062iw2uSRpvLadmwmON
-echo "Loaded ~/.tokens.sh"
