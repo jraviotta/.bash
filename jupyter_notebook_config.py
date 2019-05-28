cde7d1fc524eed3818231f233cbf1347667c00de
Your Name <jraviotta@gmail.com>
2019-05-28 17:17:42 -0400

    discarding jupyter pre-save scrub hook idea

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -18,22 +18,22 @@ import platform
 if platform.system() == 'Darwin':
     c.LabApp.browser = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=%s'
 
-def scrub_output_pre_save(model, **kwargs):
-    """scrub output before saving notebooks"""
-    # only run on notebooks
-    if model['type'] != 'notebook':
-        return
-    # only run on nbformat v4
-    if model['content']['nbformat'] != 4:
-        return
+# def scrub_output_pre_save(model, **kwargs):
+#     """scrub output before saving notebooks"""
+#     # only run on notebooks
+#     if model['type'] != 'notebook':
+#         return
+#     # only run on nbformat v4
+#     if model['content']['nbformat'] != 4:
+#         return
 
-    for cell in model['content']['cells']:
-        if cell['cell_type'] != 'code':
-            continue
-        cell['outputs'] = []
-        cell['execution_count'] = None
+#     for cell in model['content']['cells']:
+#         if cell['cell_type'] != 'code':
+#             continue
+#         cell['outputs'] = []
+#         cell['execution_count'] = None
 
-c.FileContentsManager.pre_save_hook = scrub_output_pre_save
+# c.FileContentsManager.pre_save_hook = scrub_output_pre_save
 
 import io
 import os
